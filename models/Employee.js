74f26d3db6b0825f55712b8a5e05d92d6aa7c951
Jehan Hapugalle <jehan.hapugalle@gmail.com>
2021-08-27 20:18:27 +0530

    first commit

---
+++ models/Employee.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema({
+
+    eid : {
+        type : String,
+        required : true
+    },
+    name : {
+        type : String,
+        required : true
+    },
+    gender : {
+        type : String,
+        required : true
+    },
+    date_joined : {
+        type : String,
+        required : true
+    },
+    dob : {
+        type : String,
+        required : true
+    },
+	contact : {
+        type : String,
+        required : true
+    },
+    address : {
+        type : String,
+        required : true
+    }
+})
+
+const Employee = mongoose.model("employee", employeeSchema);
+
+module.exports = Employee;
