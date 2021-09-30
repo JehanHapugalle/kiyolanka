de113f3222095a428fc72f7c73e885fb078c15ad
Lakindu Perera <86124152+LAKEY82@users.noreply.github.com>
2021-09-30 19:12:12 +0530

    Material Display update and create

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import { Machinery,MachineryOne,MachineryTwo,MachineryThree,MachineryFour} from './pages/Machinery';
-
+import { Materials,MaterialOne,MaterialTwo,MaterialThree,MaterialFour} from './pages/Material';
 
 function App() {
   return (
@@ -21,6 +21,12 @@ function App() {
          <Route path='/machine/viewmachine' exact component={MachineryTwo} />
          <Route path='/machine/expenses' exact component={MachineryThree} />
          <Route path='/machine/analysis1' exact component={MachineryFour} />
+
+         <Route path='/materials' exact component={Materials} />
+         <Route path='/materials/addmaterials' exact component={MaterialOne} />
+         <Route path='/materials/materialstock' exact component={MaterialTwo} />
+         <Route path='/materials/usedmaterials' exact component={MaterialThree} />
+         <Route path='/materials/analaysis3' exact component={MaterialFour} />
 
 
         </switch>
