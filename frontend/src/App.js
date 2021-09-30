f4016646db029ec74172644f42c02d6bce047202
kavindu100 <githmink@gmail.com>
2021-09-30 19:47:21 +0530

    transport create retrieve FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,14 @@ import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import { Machinery,MachineryOne,MachineryTwo,MachineryThree,MachineryFour} from './pages/Machinery';
-
+import { Transport,TransportOne,TransportTwo,TransportThree,} from './pages/Transport';
 
 function App() {
   return (
 
       <Router>
-      <Sidebar/>
-    
+        
+      <Sidebar/> 
 
         <switch>
 
@@ -21,6 +21,15 @@ function App() {
          <Route path='/machine/viewmachine' exact component={MachineryTwo} />
          <Route path='/machine/expenses' exact component={MachineryThree} />
          <Route path='/machine/analysis1' exact component={MachineryFour} />
+
+         
+
+         
+         <Route path='/transport' exact component={Transport} />
+         <Route path='/transport/addtransport' exact component={TransportOne} />
+         <Route path='/transport/maintenance' exact component={TransportTwo} />
+         <Route path='/transport/analysis6' exact component={TransportThree} />
+        
 
 
         </switch>
